05effc207d6c848d0e1734316a8baefe4836ba17
song-giung <rldnddl87@gmail.com>
2021-10-18 00:50:02 +0900

    initiate class, assert

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+class Item:
+    def __init__(self, name: str, price: float, quantity=0):
+        # validate received arguments
+        # can raise AssertionError
+        assert price >= 0
+        assert quantity >= 0, f"Quantity must be grater than 0 but {quantity}"
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+item1 = Item("Phone", 100, -1)
+# 다이나믹, 스크립팅 언어 객체를 생성한 이후에 필드를 추가할 수 있다.
+item1.has_num_pad = False
+print(item1.calculate_total_price())
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# __method__ : magic method
+# __init__ : 생성자 함수
+# 생성자 함수에 기본 값을 설정하면 강제가 아니게 된다.
