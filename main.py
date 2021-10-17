d64bd20386a1662bfd911261039590aa0227c56d
song-giung <rldnddl87@gmail.com>
2021-10-18 01:06:34 +0900

    instance level vs class level

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 class Item:
-    pay_rate = 0.8  # class attribute
+    pay_rate = 0.8
 
     def __init__(self, name: str, price: float, quantity=0):
-        # validate received arguments
-        # can raise AssertionError
         assert price >= 0
         assert quantity >= 0, f"Quantity must be grater than 0 but {quantity}"
 
-        # Assign to self object
-        # instance attribute
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -17,17 +13,17 @@ class Item:
         return self.price * self.quantity
 
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        # self.pay_rate vs Item.pay_rate
+        # sel_pay_rate로 선언할 경우 인스턴스 별로 pay_rate를 다르게 설정할 수 가 있다.
+        self.price = self.price * self.pay_rate # instance level => class level 로 찾는다.
 
 
 item1 = Item("Phone", 100, 0)
-# 다이나믹, 스크립팅 언어 객체를 생성한 이후에 필드를 추가할 수 있다.
+item1.apply_discount()
+print(item1.price)
 
-print(Item.pay_rate)
-print(item1.pay_rate)  # 먼저 instance attribute 를 찾아보고 없으면 클래스 attribute 를 찾아본다.
-print(Item.__dict__)  # All the attributes for Class Level
-print(item1.__dict__)  # All the attributes for Instance Level
+item2 = Item("Laptop", 1000, 0)
+item2.pay_rate = 0.7 # 인스턴스 멤버를 추가한 것이지 class attribute를 변경한 것이 아니다.
+item2.apply_discount()
+print(item2.price)
 
-# __method__ : magic method
-# __init__ : 생성자 함수
-# 생성자 함수에 기본 값을 설정하면 강제가 아니게 된다.
